7f1b7618f6e834541b7adc0ac02638abe1cbff2f
a1xsa <tsvetkov-alexey@mail.ru>
2024-10-28 07:35:03 +0300

    Console Controller

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,7 +1,10 @@
 package App;
 
+import ConsoleController.ConsoleController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsoleController app=new ConsoleController();
+        app.start();
     }
 }
