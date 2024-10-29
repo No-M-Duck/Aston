1eca251bade58e0c5f7268b7bfd7f4392d9b1465
nick-kalash777 <nick.kalash777@gmail.com>
2024-10-29 14:08:28 +0300

    added getString() to Writers instead of repeating code

--- src/main/java/output/BusWriter.java
+++ src/main/java/output/BusWriter.java
@@ -9,14 +9,17 @@ public class BusWriter extends CollectionWriter<Bus> {
     public void toFile (List<Bus> buses) {
         ArrayList<String> busesOutput = new ArrayList<>();
         for (Bus bus : buses) {
-            String busString = bus.getNumber()+delimiter+bus.getModel()+delimiter+bus.getMileage();
-            busesOutput.add(busString);
+            String busString = getString(bus);
         }
         writeCollection("buses", busesOutput);
     }
 
     public void toFile (Bus bus) {
-        String busString = bus.getNumber()+delimiter+bus.getModel()+delimiter+bus.getMileage();
+        String busString = getString(bus);
         writeElement("buses_found", busString);
+    }
+
+    private String getString(Bus bus) {
+        return bus.getNumber()+delimiter+bus.getModel()+delimiter+bus.getMileage();
     }
 }
--- src/main/java/output/UserWriter.java
+++ src/main/java/output/UserWriter.java
@@ -9,14 +9,18 @@ public class UserWriter extends CollectionWriter<User> {
     public void toFile (List<User> users) {
         ArrayList<String> usersOutput = new ArrayList<>();
         for (User user : users) {
-            String userString = user.getName()+delimiter+user.getEmail()+delimiter+user.getPassword();
+            String userString = getString(user);
             usersOutput.add(userString);
         }
         writeCollection("users", usersOutput);
     }
 
     public void toFile (User user) {
-        String userString = user.getName()+delimiter+user.getEmail()+delimiter+user.getPassword();
+        String userString = getString(user);
         writeElement("users_found", userString);
+    }
+
+    private String getString (User user) {
+        return user.getName()+delimiter+user.getEmail()+delimiter+user.getPassword();
     }
 }
